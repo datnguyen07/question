b5eb6570149901dec08d923bf352a7e6a6d1fbc4
datnguyen07 <73738282+datnguyen07@users.noreply.github.com>
2022-03-04 16:15:28 +0700

    m

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "Question for you",
+    introTitle: 'Gửi cậu',
+    introDesc: `Hãy trả lời câu hỏi.. Trả lời cho thật lòng nghe chưa`,
+    btnIntro: 'Đồng ý',
+    title: 'THÍCH ĐẠT KHÔNG 🥰',
+    desc: 'Không được tắt nha',
+    btnYes: 'Thích lắm <33',
+    btnNo: 'Không nha :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại thích mình <3',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Mình biết mà 🥰. Mình cũng thế',
+    messDesc: 'Đi ăn cùng mình nha',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/profile.php?id=100015551769104' 
+}
